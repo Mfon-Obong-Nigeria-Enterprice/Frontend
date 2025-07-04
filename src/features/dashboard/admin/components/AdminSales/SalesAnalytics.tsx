e5d120b7e4a540a0f0453724a330b200ead1ff9e
Adeleke Yusuf <adelekeadeyemi19@gmail.com>
2025-07-04 16:30:33 +0100

    feat:some tailwind css added

--- src/features/dashboard/admin/components/AdminSales/SalesAnalytics.tsx
+++ src/features/dashboard/admin/components/AdminSales/SalesAnalytics.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import SalesCategoryChart from "./SalesCategoryChart";
+import { BsArrowUp } from "react-icons/bs";
 
 const salesByCategory = [
   { name: "Building Materials", value: 45 },
@@ -55,58 +56,79 @@ const SalesAnalytics: React.FC<SalesAnalyticsProps> = ({
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 my-3">
         <Card>
-          <CardHeader className="flex flex-row items-center justify-between">
-            <CardTitle className="text-sm font-medium text-gray-500">
+          <CardHeader>
+            <CardTitle className="text-sm font-normal text-[#7D7D7D]">
               Total sales(Today)
             </CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold text-gray-900">
-              ₦{metrics.totalSales.toLocaleString()}
+            <div className="flex flex-col gap-2">
+              <div className="text-2xl font-bold text-gray-900">
+                ₦{metrics.totalSales.toLocaleString()}
+              </div>
+              <p className="text-xs text-[#2ECC71] font-normal leading-4 flex items-center">
+                <BsArrowUp />
+                15% from yesterday
+              </p>
             </div>
-            <p className="text-xs text-green-600">+15% from yesterday</p>
-          </CardContent>
-        </Card>
-        {/*  */}
-        <Card>
-          <CardHeader className="flex fle-row items-center justify-between">
-            <CardTitle className="text-sm font-medium text-gray-500">
-              Transactions
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold text-gray-900">
-              {metrics.transactionCount}
-            </div>
-            <p className="text-xs text-green-600">Today's completed sales</p>
           </CardContent>
         </Card>
         {/*  */}
         <Card>
           <CardHeader className="flex flex-row items-center justify-between">
-            <CardTitle className="text-sm font-medium text-gray-500">
+            <CardTitle className="text-sm font-normal text-[#7D7D7D]">
+              Transactions
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col gap-2">
+              <div className="text-2xl font-bold text-gray-900">
+                {metrics.transactionCount}
+              </div>
+              <p className="text-xs text-[#2ECC71] font-normal leading-4 flex items-center">
+                <BsArrowUp />
+                8% more than yesterday
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+        {/*  */}
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between">
+            <CardTitle className="text-sm font-normal text-[#7D7D7D]">
               Active clients
             </CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold text-gray-900">
-              {metrics.activeClients}
+            <div className="flex flex-col gap-2">
+              <div className="text-2xl font-bold text-gray-900">
+                {metrics.activeClients}
+              </div>
+              <p className="text-xs text-[#2ECC71] font-normal leading-4 flex items-center">
+                <BsArrowUp />
+                Today's completed sales
+              </p>
             </div>
-            <p className="text-xs text-green-600">Today's completed sales</p>
           </CardContent>
         </Card>
         {/* Average Transaction Card */}
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium text-gray-500">
-              Average Transaction
+            <CardTitle className="text-sm font-normal text-[#7D7D7D]">
+              Avg. Transactions
             </CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold text-gray-900">
-              ₦{metrics.averageTransaction.toLocaleString() || 0}
+            <div className="flex flex-col gap-2">
+              <div className="text-2xl font-bold text-gray-900">
+                ₦{metrics.averageTransaction.toLocaleString()}
+              </div>
+              <p className="text-xs text-[#F95353] flex items-center">
+                {" "}
+                <BsArrowUp />
+                %5 from last week
+              </p>
             </div>
-            <p className="text-xs text-gray-600">Per transaction value</p>
           </CardContent>
         </Card>
       </div>
@@ -117,61 +139,61 @@ const SalesAnalytics: React.FC<SalesAnalyticsProps> = ({
         <div className="lg:col-span-3">
           <Card>
             <CardHeader>
-              <CardTitle className="text-lg font-semibold text-gray-900">
+              <CardTitle className="text-[20px] font-medium text-[#1E1E1E]">
                 Top-selling products
               </CardTitle>
             </CardHeader>
             <CardContent>
               {/* Headers - Desktop Only */}
-              <div className=" gap-4 border-b border-gray-200 pb-3 grid grid-cols-4 ">
+              <div className=" border-b border-gray-200 pb-3 grid grid-cols-4 ">
                 <div>
-                  <h4 className="text-xs font-medium tracking-wide text-gray-500">
+                  <h4 className=" xl:text-base lg:text-sm md:text-xs font-normal tracking-wide text-[#1E1E1E]">
                     Product
                   </h4>
                 </div>
                 <div>
-                  <h4 className="text-xs font-medium tracking-wide text-gray-500">
+                  <h4 className=" xl:text-base lg:text-sm md:text-xs font-normal tracking-wide text-[#1E1E1E]">
                     Units Sold
                   </h4>
                 </div>
                 <div>
-                  <h4 className="text-xs font-medium tracking-wide text-gray-500">
+                  <h4 className=" xl:text-base lg:text-sm md:text-xs font-normal tracking-wide text-[#1E1E1E]">
                     Revenue Generated
                   </h4>
                 </div>
                 <div>
-                  <h4 className="text-xs font-medium tracking-wide text-gray-500">
+                  <h4 className=" xl:text-base lg:text-sm md:text-xs font-normal tracking-wide text-[#1E1E1E]">
                     Category
                   </h4>
                 </div>
               </div>
               {topProduct && topProduct.length > 0 ? (
-                <div className="space-y-0 ">
+                <div className="space-y-0 divide-y divide-gray-200 md:divide-y-0 md:divide-x-4">
                   <>
                     {topProduct.map((product) => (
                       <div
                         key={product.prodName}
-                        className={`border-b-2 border-gray-100 last:border-b-0 md:border-b-0 $`}
+                        className={`border-b-2 border-gray-100 last:border-b-0 md:border-b-0 `}
                       >
                         {/* Desktop layout */}
-                        <div className="hidden gap-4 py-3 md:grid md:grid-cols-4 px-4 first:pl-0">
+                        <div className="hidden py-3 md:grid md:grid-cols-4 first:pl-0">
                           <div>
-                            <h4 className="text-sm font-medium text-gray-700 flex items-center ">
+                            <h4 className="text-sm font-normal text-[#444444]  ">
                               {product.prodName}
                             </h4>
                           </div>
-                          <div className="flex items-center">
-                            <p className="text-xs text-gray-500">
+                          <div className="">
+                            <p className="text-sm text-[#444444] font-normal">
                               {product.soldUnit} bags
                             </p>
                           </div>
                           <div className="flex items-center ">
-                            <p className="text-lg text-gray-500">
+                            <p className="text-sm text-[#444444] font-normal">
                               {product.revenue.toLocaleString()}
                             </p>
                           </div>
                           <div>
-                            <p className="text-xs text-gray-500">
+                            <p className="text-sm text-[#444444] font-normal">
                               {product.category}
                             </p>
                           </div>
