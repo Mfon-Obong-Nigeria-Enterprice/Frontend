a63911fc21111bdc9e36cd578c70444d608d7702
ubekm <ubekmichael@gmail.com>
2025-07-11 18:56:46 +0100

    Updated maintainer dashboard - sidebar and overview

--- src/features/dashboard/maintainer/MaintainerDashboard.tsx
+++ src/features/dashboard/maintainer/MaintainerDashboard.tsx
@@ -6,8 +6,23 @@ import Stats, {
 } from "@/features/dashboard/maintainer/components/Stats";
 import { Button } from "@/components/ui/Button";
 import OutstandingBalance from "@/features/dashboard/maintainer/components/OutstandingBalance";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+
+type Transaction = {
+  invoice: string;
+  client: string;
+  type: "Debit" | "Credit" | "Partial";
+  amount: string;
+  location: string;
+  balance: string;
+  staff: string;
+  date: string;
+  time: string;
+};
 
 const MaintainerDashboard = () => {
+  const navigate = useNavigate();
   const stats: StatCard[] = [
     {
       heading: "Total Sales (Today)",
@@ -28,54 +43,46 @@ const MaintainerDashboard = () => {
       color: "orange",
     },
   ];
-  const transactionData: Transaction[] = [
+  const transactionData = [
     {
       invoice: "INV-2025-001",
-      client: "Akpan construction",
-
+      date: "5/19/2025",
+      time: "11:40 AM",
+      items: "5x Nails",
       type: "Debit",
-      amount: "-₦ 250,000",
+      amount: "-₦12,750",
       location: "Main Office",
-      balance: "-₦ 450,000",
-      staff: "John Doe",
-      date: "5/25/2025",
-      time: "10:00 AM",
+      balance: "₦0.00",
     },
     {
       invoice: "INV-2025-002",
-      client: "Effion builders",
-
-      type: "Partial",
-      amount: "₦ 100,000",
+      date: "5/19/2025",
+      time: "11:25 AM",
+      items: "10x Cement",
+      type: "Credit",
+      amount: "+₦150,000",
       location: "Warehouse",
-      balance: "-₦ 450,000",
-      staff: "Jane Smith",
-      date: "5/21/2025",
-      time: "11:00 AM",
+      balance: "+₦150,000",
     },
     {
-      invoice: "INV-2025-003",
-      client: "Udom properties",
-
-      type: "Credit",
-      amount: "₦ 75,000",
+      invoice: "WB-2025-003",
+      date: "5/19/2025",
+      time: "9:15 AM",
+      items: "25x Steel Rods",
+      type: "Partial",
+      amount: "+₦225,000",
       location: "Main Office",
-      balance: "-₦ 450,000",
-      staff: "Mike Johnson",
-      date: "5/25/2025",
-      time: "10:00 AM",
+      balance: "-₦5,000",
     },
     {
       invoice: "INV-2025-004",
-      client: "Aniekan & sons",
-
+      date: "5/19/2025",
+      time: "8:10 AM",
+      items: "3x Nails",
       type: "Debit",
-      amount: "-₦ 255,000",
+      amount: "-₦7,500",
       location: "Warehouse",
-      balance: "-₦ 450,000",
-      staff: "Sara Wilson",
-      date: "5/04/2025",
-      time: "10:00 AM",
+      balance: "₦0.00",
     },
   ];
   return (
@@ -88,43 +95,43 @@ const MaintainerDashboard = () => {
       </div>
       <Stats data={stats} />
       <div className="bg-white border mt-7 rounded-xl shadow-xl mx-2 px-2">
-        <h5 className="text-[#1E1E1E] text-xl font-medium py-6 pl-8">
+        <h5 className="text-[#1E1E1E] text-xl font-medium py-6 pl-7">
           Recent Transactions
         </h5>
-        <table className="w-full">
-          <thead className="bg-[#F5F5F5] border border-[#d9d9d9]">
+        <table className="w-full pl-4">
+          <thead className="bg-[#F5F5F5] border border-[#d9d9d9] text-left pl-8">
             <tr>
-              <th className="py-3 text-base text-[#333333] font-normal text-center">
+              <th className="py-3 text-base text-[#333333] font-normal pl-4">
                 Invoice
               </th>
-              <th className="py-3 text-base text-[#333333] font-normal text-center">
+              <th className="py-3 text-base text-[#333333] font-normal pl-4">
                 Date/Time
               </th>
-              <th className="py-3 text-base text-[#333333] font-normal text-center">
-                Clients
+              <th className="py-3 text-base text-[#333333] font-normal pl-4">
+                Items
               </th>
-              <th className="py-3 text-base text-[#333333] font-normal text-center">
+              <th className="py-3 text-base text-[#333333] font-normal pl-4">
                 Type
               </th>
-              <th className="py-3 text-base text-[#333333] font-normal text-center">
+              <th className="py-3 text-base text-[#333333] font-normal pl-4">
                 Amount
               </th>
-              <th className="py-3 text-base text-[#333333] font-normal text-center">
+              <th className="py-3 text-base text-[#333333] font-normal pl-4">
                 Location
               </th>
-              <th className="py-3 text-base text-[#333333] font-normal text-center">
+              <th className="py-3 text-base text-[#333333] font-normal pl-4">
                 Balance
               </th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="pl-4">
             {transactionData.map((transaction, i) => {
               return (
                 <tr key={i} className="border-b border-[#d9d9d9]">
-                  <td className=" text-center text-[#444444] text-sm font-normal py-3">
+                  <td className="text-[#444444] text-sm font-normal py-3 pl-4">
                     {transaction.invoice}
                   </td>
-                  <td className="flex flex-col text-center  font-normal py-3">
+                  <td className="flex flex-col font-normal py-3 pl-4">
                     <span className="text-xs text-[#444444]">
                       {" "}
                       {transaction.date}
@@ -134,10 +141,10 @@ const MaintainerDashboard = () => {
                       {transaction.time}
                     </span>
                   </td>
-                  <td className=" text-center text-[#444444] text-sm font-normal py-3">
-                    {transaction.client}
+                  <td className="text-[#444444] text-sm font-normal py-3 pl-4">
+                    {transaction.items}
                   </td>
-                  <td className="text-center">
+                  <td className="pl-4">
                     <span
                       className={`border text-sm py-1.5 px-3 rounded-[6.25rem] ${
                         transaction.type === "Debit"
@@ -152,7 +159,7 @@ const MaintainerDashboard = () => {
                   </td>
 
                   <td
-                    className={`text-sm text-center ${
+                    className={`text-sm pl-4  ${
                       transaction.amount.includes("-")
                         ? "text-[#F95353]"
                         : "text-[#2ECC71]"
@@ -160,11 +167,11 @@ const MaintainerDashboard = () => {
                   >
                     {transaction.amount}
                   </td>
-                  <td className={`text-sm text-center ${"text-[#444444]"}`}>
+                  <td className="text-sm pl-4 text-[#444444]">
                     {transaction.location}
                   </td>
                   <td
-                    className={`text-sm text-center ${
+                    className={`text-sm pl-4  ${
                       transaction.balance.includes("-")
                         ? "text-[#F95353]"
                         : transaction.balance.startsWith("₦0")
@@ -179,14 +186,18 @@ const MaintainerDashboard = () => {
             })}
           </tbody>
         </table>
-        <div className="flex justify-between items-center bg-[#f0f0f3] mt-[2dvh] mb-2">
+        <div className="flex justify-between items-center bg-[#f0f0f3] mt-[2dvh] mb-3">
           <div>
             <Button
               onClick={() => navigate("/maintainer/dashboard/clients")}
-              text="View all Balances"
               variant="outline"
-            />
+              size={"lg"}
+              className="text-[#3D80FF] text-base "
+            >
+              View all Balances
+            </Button>
           </div>
+          <MdKeyboardArrowRight size={24} className="mr-5 text-[#3D80FF]" />
         </div>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-[60fr_40fr] gap-5 mt-5"></div>
