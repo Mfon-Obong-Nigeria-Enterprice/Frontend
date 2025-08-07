e5aec022ee4a468a4dee2008b69125eef5969d65
Ejiro-Frances <francesejiro@yahoo.com>
2025-08-07 09:23:28 +0100

    removed dev switcher

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button";
 import MobileError from "./MobileError";
 import SupportFeedback from "../../components/SupportFeedback";
 import { useAuthStore } from "@/stores/useAuthStore";
-import DevRoleSwitcher from "../../components/RoleSwitcher";
+// import DevRoleSwitcher from "../../components/RoleSwitcher";
 
 type LoginFormInputs = {
   username: string;
@@ -187,11 +187,11 @@ const Login = () => {
       </section>
 
       {/* ✅ Dev Role Switcher (only visible in development) */}
-      {import.meta.env.DEV && (
+      {/** {import.meta.env.DEV && (
         <div className="mt-4">
           <DevRoleSwitcher />
         </div>
-      )}
+      )}**/}
     </main>
   );
 };
