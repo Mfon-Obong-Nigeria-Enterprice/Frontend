ddb53290fa68b935bf99598f34d6d283246c5d65
Ejiro-Frances <francesejiro@yahoo.com>
2025-08-17 22:21:29 +0100

    fixed bug

--- src/layout/RootInterceptorLayout.tsx
+++ src/layout/RootInterceptorLayout.tsx
@@ -1,13 +1,13 @@
-import { Outlet, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import { setupInterceptors } from "@/services/baseApi";
+// import { Outlet, useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+// import { setupInterceptors } from "@/services/baseApi";
 
-export default function RootInterceptorLayout() {
-  const navigate = useNavigate();
+// export default function RootInterceptorLayout() {
+//   const navigate = useNavigate();
 
-  useEffect(() => {
-    setupInterceptors(navigate);
-  }, [navigate]);
+//   useEffect(() => {
+//     setupInterceptors(navigate);
+//   }, [navigate]);
 
-  return <Outlet />; // renders nested routes
-}
+//   return <Outlet />; // renders nested routes
+// }
