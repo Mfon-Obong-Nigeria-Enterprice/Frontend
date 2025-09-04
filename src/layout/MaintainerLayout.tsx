089e3689bc54a4dadbfa25c252c3adbe10f21b1d
Ejiro-Frances <francesejiro@yahoo.com>
2025-09-04 12:33:34 +0100

    added modals to user details page

--- src/layout/MaintainerLayout.tsx
+++ src/layout/MaintainerLayout.tsx
@@ -6,9 +6,13 @@ import { useLocation } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import MaintainerSidebar from "@/features/sidebar/MaintainerSidebar";
 import { AppProvider } from "@/providers/AppProvider";
+import { useModalStore } from "@/stores/useModalStore";
+import DeleteUserModal from "@/features/dashboard/shared/usermanagement/modals/deleteusermodal";
+import UserStatusModal from "@/features/dashboard/shared/usermanagement/modals/userstatusmodal";
 
 const MaintainerLayout = () => {
   const pathname = useLocation();
+  const { deleteModal, statusModal, closeModals } = useModalStore();
 
   return (
     <SidebarProvider>
@@ -17,8 +21,8 @@ const MaintainerLayout = () => {
         <Header userRole="maintainer" />
         <SidebarTrigger className="fixed z-50" />
         <div
-          className={`   min-h-[90dvh] ${
-            pathname.pathname.endsWith("/user")
+          className={`min-h-[90dvh] ${
+            pathname.pathname.endsWith("/user-management")
               ? "bg-[#f5f5f5] xl:bg-white mt-[4rem] border border-[#D9D9D9]"
               : "bg-[#f5f5f5] p-10 mt-[3rem]"
           }`}
@@ -27,6 +31,18 @@ const MaintainerLayout = () => {
             <Outlet />
           </AppProvider>
         </div>
+        {/* Global modals for maintainer */}
+        {deleteModal && (
+          <DeleteUserModal user={deleteModal} onClose={closeModals} />
+        )}
+
+        {statusModal && (
+          <UserStatusModal
+            user={{ id: statusModal.id, name: statusModal.name }}
+            action={statusModal.action}
+            onClose={closeModals}
+          />
+        )}
       </div>
     </SidebarProvider>
   );
