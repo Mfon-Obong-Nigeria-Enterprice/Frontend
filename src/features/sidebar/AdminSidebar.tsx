e7156e6f4a016beef6cc81208e7f0460e6e28e4d
Ejiro-Frances <francesejiro@yahoo.com>
2025-09-12 12:10:37 +0100

    resolved with main

--- src/features/sidebar/AdminSidebar.tsx
+++ src/features/sidebar/AdminSidebar.tsx
@@ -122,7 +122,7 @@ export const AdminSidebarWithModal = () => {
       logoutMutation.mutate(); // trigger logout
       setIsLoading(false);
       setShowModal(false);
-    }, 500);
+    }, 100);
   };
 
   return (
